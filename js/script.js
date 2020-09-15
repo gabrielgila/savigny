e95f06b092c9f976ae675e10d8963d4cde8fe3c8
mgila <marianogila@gmail.com>
2020-09-14 20:08:33 -0700

    contact ok

--- js/script.js
+++ js/script.js
@@ -2,15 +2,16 @@
 $(window).scroll(function () {
     let position = $(this).scrollTop();
     if (position >= 100) {
-        $(".nav-menu").addClass("costum-navbar");
-    } else {
-        $(".nav-menu").removeClass("costum-navbar");
-    }
+        $(".nav-menu").addClass("costum-navbar pt-2 pb-2");
+        } else {
+            $(".nav-menu").removeClass("costum-navbar pt-2 pb-2");
+        }
 });
 
 // ===== HAMBURGER BUTTON ===== //
 $(document).ready(function () {
     $(".nav-button").click(function () {
         $(".nav-button").toggleClass("change");
+        $(".nav-menu").toggleClass("costum-navbar pt-2 pb-2");
     });
 });

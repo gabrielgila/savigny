5873a65549dc60e70279b6b4441c0f24336cf016
mgila <marianogila@gmail.com>
2020-09-24 22:27:56 -0700

    footer ok

--- js/script.js
+++ js/script.js
@@ -1,14 +1,18 @@
-// ===== NAVBAR ===== //
+// === NAVBAR === //
 $(window).scroll(function () {
     let position = $(this).scrollTop();
-    if (position >= 100) {
-        $(".nav-menu").addClass("costum-navbar pt-2 pb-2");
+    if (window.innerWidth > 768) {
+        console.log("uno");
+        if (position >= 100) {
+            console.log("dos");
+            $(".nav-menu").addClass("costum-navbar pt-2 pb-2");
         } else {
             $(".nav-menu").removeClass("costum-navbar pt-2 pb-2");
         }
+    }
 });
 
-// ===== HAMBURGER BUTTON ===== //
+// === HAMBURGER BUTTON === //
 $(document).ready(function () {
     $(".nav-button").click(function () {
         $(".nav-button").toggleClass("change");

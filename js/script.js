6c95011c016514192d4cdd462f8c136dce999cee
mgila <marianogila@gmail.com>
2020-09-29 12:08:55 -0700

    video in process

--- js/script.js
+++ js/script.js
@@ -3,6 +3,34 @@ $(window).bind('resize', function(e) {
     this.location.reload(false);
 });
 
+// === VIDEO HOME === //
+$(document).ready(function () {
+    function scaleToFill() {
+        $('video.video-background').each(function(index, videoTag) {
+            var $video = $(videoTag),
+                videoRatio = videoTag.videoWidth / videoTag.videoHeight,
+                tagRatio = $video.width() / $video.height(),
+                val;
+            if (videoRatio < tagRatio) {
+                val = tagRatio / videoRatio * 1.02; <!-- size increased by 2% because value is not fine enough and sometimes leaves a couple of white pixels at the edges -->
+            } else if (tagRatio < videoRatio) {
+                val =
+                    videoRatio / tagRatio * 1.02;
+            }
+            $video.css('transform','scale(' + val  + ',' + val + ')');
+        });
+    }
+
+    $(function () {
+        scaleToFill();
+        $('.video-background').on('loadeddata', scaleToFill);
+        $(window).resize(function() {
+            scaleToFill();
+        });
+    });
+};
+
+
 // === NAVBAR === //
 $(window).scroll(function () {
     let position = $(this).scrollTop();

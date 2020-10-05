6f8c1f01bd3f8d0000c9eaf62e8216dbfb8e5100
mgila <marianogila@gmail.com>
2020-10-04 21:26:06 -0700

    test no scroll

--- js/script.js
+++ js/script.js
@@ -27,6 +27,7 @@ function ios () {
         $("#headerContentMobile").removeClass("header-image-background");
         $(".video-container").show();
     }
+
     if (navigator.userAgent.search("Safari") >= 0 && navigator.userAgent.search("Chrome") < 0) {
         $(".source-video").replaceWith($('<source src="video/homeVideo.mp4" type="video/mp4">'));
     } else {
@@ -34,21 +35,22 @@ function ios () {
 }
 
 $(document).ready(function () {
+
     // === NAVBAR === //
-    $(window).scroll(function () {
-        let position = $(this).scrollTop();
-        if (window.innerWidth > 768) {
-            if (position >= 100) {
-                $(".nav-menu").addClass("costum-navbar pt-2 pb-2");
-                $(".dulani").removeClass("dark-text");
-                $(".dulani-logo").removeClass("white-fill");
-            } else {
-                $(".nav-menu").removeClass("costum-navbar pt-2 pb-2");
-                $(".dulani").addClass("dark-text");
-                $(".dulani-logo").addClass("white-fill");
-            }
-        }
-    });
+    //$(window).scroll(function () {
+      //  let position = $(this).scrollTop();
+        //if (window.innerWidth > 768) {
+          //  if (position >= 100) {
+            //    $(".nav-menu").addClass("costum-navbar pt-2 pb-2");
+              //  $(".dulani").removeClass("dark-text");
+                //$(".dulani-logo").removeClass("white-fill");
+            //} else {
+              //  $(".nav-menu").removeClass("costum-navbar pt-2 pb-2");
+               // $(".dulani").addClass("dark-text");
+               // $(".dulani-logo").addClass("white-fill");
+            //}
+        //}
+    //});
 
     internetExporer();
     ios();
